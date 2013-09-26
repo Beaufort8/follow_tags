e7bc7f68ba52ba5b80c3f85b9bc752f60e5c571e
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-26 10:57:33 +0200

    code cleanup

--- action/save.php
+++ action/save.php
@@ -1,29 +1,19 @@
 <?php
 /**
-* Save /Change Action for Help Plugin
-*
+* Save / Change Action
 **/
 
-
 //Get Tags from Taginput
-$follow_tag_input  = get_input('followtags');
+$follow_tag_input = get_input('followtags');
 
 //Get Notification Settings
-$notify_input  =  get_input('notifyfollow');
-
+$notify_input = get_input('notifyfollow');
 
 //Save the FollowTags in the TagObj
-if(!saveFollowTags($follow_tag_input,getID(elgg_get_logged_in_user_guid()),$notify_input)){
+if (!saveFollowTags($follow_tag_input, getID(elgg_get_logged_in_user_guid()), $notify_input)) {
     register_error(elgg_echo("follow_tags:save:error"));
-}else{
+} else {
 	system_message(elgg_echo("follow_tags:save:message"));
 }
 
 forward(REFERER);
-
-
-
-
-
-
-
