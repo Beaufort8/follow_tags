01f4a6c33b4cc25fb46903468f19e38bd9946313
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-09-26 10:48:31 +0200

    no need for all those spaces

--- views/default/plugins/follow_tags/settings.php
+++ views/default/plugins/follow_tags/settings.php
@@ -1,8 +1,6 @@
 <?php
-/**
- * Groups plugin settings
- */
- $yesno_options = array(
+
+$yesno_options = array(
 		"yes" => elgg_echo("follow_tags:option:yes"),
 		"no" => elgg_echo("follow_tags:option:no")
 );
@@ -16,30 +14,22 @@ $char_options = array(
 		"5" => elgg_echo("5"),
 );
 
-
- 
 echo "<div>";
 echo elgg_echo('follow_tags:settings:autocomplete');
 echo elgg_view("input/dropdown", array("name" => "params[autocomplete]", "options_values" => $yesno_options, "value" => $vars['entity']->autocomplete));
 echo "</div>";
-
-
 
 echo "<div>";
 echo elgg_echo('follow_tags:settings:minChar');
 echo elgg_view("input/dropdown", array("name" => "params[minChar]", "options_values" => $char_options, "value" => $vars['entity']->minChar));
 echo "</div>";
 
-
 echo "<div>";
 echo elgg_echo('follow_tags:settings:threshold');
 echo elgg_view("input/dropdown", array("name" => "params[threshold]", "options_values" => $char_options, "value" => $vars['entity']->threshold));
 echo "</div>";
 
-
 echo "<div>";
 echo elgg_echo('follow_tags:settings:defaultTags');
 echo elgg_view("input/text", array("name" => "params[defaultTags]", "value" => $vars['entity']->defaultTags));
 echo "</div>";
-
-
